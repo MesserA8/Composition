abedde3499e179a9a49c86923bd1566991b056d2
MesserA8 <atihon09@gmail.com>
2022-09-06 10:15:26 +0300

    serializable removed, parcelable added

--- app/src/main/java/com/messer_amd/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/messer_amd/composition/presentation/ChooseLevelFragment.kt
@@ -7,12 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.messer_amd.composition.R
 import com.messer_amd.composition.databinding.FragmentChooseLevelBinding
+import com.messer_amd.composition.domain.entity.Level
 
 class ChooseLevelFragment : Fragment() {
 
     private var _binding: FragmentChooseLevelBinding? = null
     private val binding: FragmentChooseLevelBinding
-    get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
 
 
     override fun onCreateView(
@@ -26,22 +27,40 @@ class ChooseLevelFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonLevelTest.setOnClickListener {
-
+        with(binding) {
+            buttonLevelTest.setOnClickListener {
+                launchGameFragment(Level.TEST)
+            }
+            buttonLevelEasy.setOnClickListener {
+                launchGameFragment(Level.EASY)
+            }
+            buttonLevelNormal.setOnClickListener {
+                launchGameFragment(Level.NORMAL)
+            }
+            buttonLevelHard.setOnClickListener {
+                launchGameFragment(Level.HARD)
+            }
         }
-        binding.buttonLevelEasy.setOnClickListener {
+    }
 
-        }
-        binding.buttonLevelNormal.setOnClickListener {
-
-        }
-        binding.buttonLevelHard.setOnClickListener {
-
-        }
+    private fun launchGameFragment(level: Level) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(level))
+            .addToBackStack(GameFragment.NAME)
+            .commit()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+
+        const val NAME = "ChooseLevelFragment"
+
+        fun newInstance(): ChooseLevelFragment {
+            return ChooseLevelFragment()
+        }
     }
 }
--- app/src/main/java/com/messer_amd/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/messer_amd/composition/presentation/WelcomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.messer_amd.composition.R
 import com.messer_amd.composition.databinding.FragmentWelcomeBinding
 
+
 class WelcomeFragment : Fragment() {
 
     private var _binding: FragmentWelcomeBinding? = null
@@ -26,8 +27,15 @@ class WelcomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonUnderstand.setOnClickListener {
-
+           launchChooseLevelFragment()
         }
+    }
+
+    private fun launchChooseLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ChooseLevelFragment.newInstance())
+            .addToBackStack(ChooseLevelFragment.NAME)
+            .commit()
     }
 
     override fun onDestroyView() {
